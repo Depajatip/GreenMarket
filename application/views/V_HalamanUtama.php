9802256c77f07f436c371537cdc6af329552f31d
Depaa <defajati0@gmail.com>
2025-07-24 10:48:39 +0700

    feat: configuration MVC and Config

---
+++ application/views/V_HalamanUtama.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome To Green Mart</title>
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/style.css'); ?>">
+</head>
+<body>
+<div id="header">
+	tes
+</div>
+</body>
+</html>
