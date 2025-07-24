6001789ee7ba1cb9691c869caa23dbcba793a63d
Depaa <defajati0@gmail.com>
2025-07-24 11:43:29 +0700

    feat: update View halaman utama

--- application/views/V_HalamanUtama.php
+++ application/views/V_HalamanUtama.php
@@ -1,12 +1,34 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>Welcome To Green Mart</title>
 	<link rel="stylesheet" href="<?php echo base_url('assets/css/style.css'); ?>">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 </head>
-<body>
-<div id="header">
-	tes
-</div>
+
+<body id="body">
+	<div id="header">
+		<nav class="navbar navbar-expand-lg">
+			<div class="container">
+				<img src="logoGM.png" class="rounded-circle mb-4" style="height: 30px;" alt="">
+				<a class="navbar-brand fw-bold" style="font-size: 20px;" href="#">GreenMarket</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+					<ul class="navbar-nav" style="gap: 20px">
+						<li class="nav-item"><a class="nav-link active" href="#home">Home</a></li>
+						<li class="nav-item"><a class="nav-link" href="#feature">Feature</a></li>
+						<li class="nav-item"><a class="nav-link" href="#comunity">Comunity</a></li>
+						<li class="nav-item"><a class="nav-link" href="#blog">Blog</a></li>
+						<li class="nav-item"><a class="nav-link" href="#pricing">Pricing</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</div>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+
 </html>
